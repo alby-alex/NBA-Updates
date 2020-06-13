6dafc9b07830d840274521c9b78a96e63e3502fb
Alby Alex <albysalex@gmail.com>
2020-06-13 11:07:45 -0400

    finished part 2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import twitter
 import praw
+import time
 def post_reddit(tweet):
     if "Woj" in tweet.user.name:
         name = "[Wojnarowski] "
@@ -11,7 +12,7 @@ def post_reddit(tweet):
 
     r = praw.Reddit('bot1')
     to_submit = r.subreddit("nba")
-    to_submit.submit(title = content, url = link, validate_on_submit=True)
+    to_submit.submit(title = content, url = link)
 
 
 f = open('config.ini', 'r+')
@@ -23,10 +24,16 @@ api = twitter.Api(consumer_key=k[0][:-1],
 x = api.GetListTimeline(list_id=1271124228469907456, include_rts=False)
 last_tweet = set()
 last_tweet.add(x[0].text)
+count = 0
 while True: 
     x = api.GetListTimeline(list_id=1271124228469907456, include_rts=False)
+    if count % 100 == 0:
+        print(str(count) + ": "+ str(x))
     if x[0].text not in last_tweet:
-        post_reddit(x[0])
+        if "ESPN story" not in x[0].text and "@TheAthleticNBA:" not in x[0].text and "The Woj Pod" not in x[0].text and "@SportsCenter" not in x[0].text:
+            post_reddit(x[0])
         last_tweet.add(x[0].text)
         print("{}: {}".format(x[0].user.name, x[0].text))
+    count += 1 
+    time.sleep(1)
     
