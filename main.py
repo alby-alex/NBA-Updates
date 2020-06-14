42460c389682cc9acd061e778748b092edac0306
Alby Alex <albysalex@gmail.com>
2020-06-13 21:36:13 -0400

    finished part 2

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import twitter
 import praw
 import time
-def post_reddit(tweet):
-    if "Woj" in tweet.user.name:
-        name = "[Wojnarowski] "
-        link = "https://twitter.com/wojespn/status/{}".format(tweet.id)
-    else:
-        name = "[Charania] "
-        link = "https://twitter.com/ShamsCharania/status/{}".format(tweet.id)
-    content = name + tweet.text
+import re
+import sys
+def post_reddit(tweet, link):
 
-    r = praw.Reddit('bot1')
-    to_submit = r.subreddit("nba")
-    to_submit.submit(title = content, url = link)
+    content = name + tweet.text
+    index = content.index('http')
+    content = content[0:index]
+    print(content)
+    # r = praw.Reddit('bot1')
+    # to_submit = r.subreddit("nba")
+    # to_submit.submit(title = content, url = link)
 
 
 f = open('config.ini', 'r+')
@@ -20,20 +19,9 @@ k = f.readlines()
 api = twitter.Api(consumer_key=k[0][:-1],
                   consumer_secret=k[1][:-1],
                   access_token_key=k[2][:-1],
-                  access_token_secret=k[3][:-1])
-x = api.GetListTimeline(list_id=1271124228469907456, include_rts=False)
-last_tweet = set()
-last_tweet.add(x[0].text)
-count = 0
-while True: 
-    x = api.GetListTimeline(list_id=1271124228469907456, include_rts=False)
-    if count % 100 == 0:
-        print(str(count) + ": "+ str(x))
-    if x[0].text not in last_tweet:
-        if "ESPN story" not in x[0].text and "@TheAthleticNBA:" not in x[0].text and "The Woj Pod" not in x[0].text and "@SportsCenter" not in x[0].text:
-            post_reddit(x[0])
-        last_tweet.add(x[0].text)
-        print("{}: {}".format(x[0].user.name, x[0].text))
-    count += 1 
-    time.sleep(1)
+                  access_token_secret=k[3][:-1], 
+                  tweet_mode = 'extended')
+tweet_id = int(re.findall(r'\d+', sys.argv[1])[0])
+x = api.GetStatus(id = tweet_id)
+post_reddit(x, sys.argv[1])
     
