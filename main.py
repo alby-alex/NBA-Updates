b3432ce0eda9136715113a9648fe7246f1322c94
Alby Alex <albysalex@gmail.com>
2020-06-13 21:47:25 -0400

    finished part 2

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def post_reddit(tweet, link):
     content = name + tweet.text
     index = content.index('http')
     content = content[0:index]
+    z = open('success.txt', 'w+')
+    z.write(content)
     print(content)
     # r = praw.Reddit('bot1')
     # to_submit = r.subreddit("nba")
@@ -19,9 +21,9 @@ k = f.readlines()
 api = twitter.Api(consumer_key=k[0][:-1],
                   consumer_secret=k[1][:-1],
                   access_token_key=k[2][:-1],
-                  access_token_secret=k[3][:-1], 
+                  access_token_secret=nek[3][:-1], 
                   tweet_mode = 'extended')
 tweet_id = int(re.findall(r'\d+', sys.argv[1])[0])
-x = api.GetStatus(id = tweet_id)
+x = api.GetStatus(status_id = tweet_id)
 post_reddit(x, sys.argv[1])
     
